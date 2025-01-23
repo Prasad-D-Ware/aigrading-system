972cdd11e7c1f49fd2458c63769d64186e6a4dc2
Prasad-D-Ware <prasadware4216@gmail.com>
2025-01-24 00:04:25 +0530

    ft: title and login signup routes added

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+
+ export default function Login() {
+  return (
+    <div className="font-bold flex justify-center text-2xl">
+       <div>Login Page</div>
+     </div>
+  );
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+
+ export default function Login() {
+  return (
+    <div className="font-bold flex justify-center text-2xl">
+       <div>Sign Up Page</div>
+     </div>
+  );
+}
