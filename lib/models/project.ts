71f7b9772f4644fe515ec8d7a5b2602baf1a1f91
Prasad-D-Ware <prasadware42116@gmail.com>
2025-01-28 00:00:02 +0530

    rf : ft: project model

---
+++ lib/models/project.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectSchema = new Schema(
+    {
+        project_id : {
+            type : String,
+            unique : true
+        },
+        project_name : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        full_name : {
+            type : String,
+            unique: true
+        },
+        project_url : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        project_creator : {
+            type : String,
+            unique :true
+        },
+        description :{
+            type :String,
+            unique : true
+        },
+        no_of_contributers : {
+            type : Number
+        },
+        created_at : {
+            type : Date
+        },
+        updated_at : {
+            type : Date
+        },
+        forks : {
+            type : String
+        },
+        stars : {
+            type : String
+        },
+        visibility : {
+            type : String
+        },
+        language : {
+            type : String
+        },
+        contributers : {
+            type : [String]
+        },
+    }
+);
+
+
+const Project = mongoose.models.Project || mongoose.model("Project", projectSchema);
+
+export default Project;
