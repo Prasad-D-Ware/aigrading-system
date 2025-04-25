27dd670303acfea4d1255c8b09dc2abe941a63d4
Prasad-D-Ware <prasadware42116@gmail.com>
2025-04-25 09:41:30 +0530

    rf : added server runtime timeout

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // This will ignore ESLint errors during builds
   },
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  // Increase function timeout
+  serverRuntimeConfig: {
+    functionTimeout: 30, // 30 seconds (this is the maximum for some serverless platforms)
+  }
 };
 
 export default nextConfig;
